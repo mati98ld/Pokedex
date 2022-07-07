ccde18b36dbfa95ad0655499791c92f71d126967
Maxi <diazmaxi.sis@gmail.com>
2022-07-06 22:46:51 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let pokemonNames = document.querySelector("#lista")
+
+fetch("https://pokeapi.co/api/v2/pokemon/")
+    .then(response => response.json())
+    .then(function (response) {
+        for (let i = 0; i < response.results.length; i++) {
+            let nombrePokemon = response.results[i].name
+            let urlDetalle = response.results[i].url
+            let li = document.createElement("li")
+            li.innerHTML = nombrePokemon
+            pokemonNames.appendChild(li)
+            li.onclick = function () {
+                fetch(urlDetalle)
+                    .then(response => response.json())
+                    .then(function (response) {
+                        let altura = response.height
+                        let peso = response.weight
+                        let imagen = response.sprites.other.dream_world.front_default
+                        let $altura = document.querySelector("#altura")
+                        let $peso = document.querySelector("#peso")
+                        let $imagen = document.querySelector("#imagen")
+                        let $nombre = document.querySelector("#nombre")
+                        $nombre.innerHTML = nombrePokemon
+                        $altura.innerHTML = altura
+                        $peso.innerHTML = peso
+                        $imagen.style.backgroundImage = "url(" + imagen+ ")";
+                        let tipos = response.types
+                        let $tipos = document.querySelector("#tipos")
+                        $tipos.innerHTML = ""
+                        for (let i = 0; i < tipos.length; i++) {
+                            let nombreTipo = tipos[i].type.name
+                            let li = document.createElement("li")
+                            li.innerHTML = nombreTipo
+                            $tipos.appendChild(li)
+                        }
+                        
+
+                    })
+            }
+        }
+    })
+
